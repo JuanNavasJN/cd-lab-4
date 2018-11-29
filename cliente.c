76a47966d2386d43fe141c9e3eb8bdd4fe952282
Juan Navas <juannavas@MacBook-Pro-de-Juan.local>
2018-11-29 08:31:01 -0400

    Update

--- cliente.c
+++ cliente.c
@@ -4,23 +4,25 @@
 #include<arpa/inet.h>	//inet_addr
 #include<stdio.h>	//printf
 #include <stdlib.h>
+#include<unistd.h>    //write
+#include <string.h> 
 
 //#defien PORT 3005
 //#define BACKLOG 2
 
 int main (int argc, char *argv[]){
     int id; //Descriptores servidor, cliente
-    char buffer[100] = "hola";
+    char buffer[500];
 
     struct sockaddr_in server; //int servidor
 
     struct hosten *he; // lo necesitare para el nombre de host o la ip
 
     //Necesario para obtener la ip. Existe otra funcion para esto que quiza necesiten...
-    if((he=gethostbyname(argv[1])) == NULL){
-        printf("error de gethostbyname()\n");
-        exit(-1);
-    }
+    // if((he=gethostbyname(argv[1])) == NULL){
+    //     printf("error de gethostbyname()\n");
+    //     exit(-1);
+    // }
 
     int port;
     sscanf(argv[2], "%d", &port);
@@ -29,26 +31,42 @@ int main (int argc, char *argv[]){
 
     server.sin_family = AF_INET;
 
-    server.sin_addr = *((struct in_addr *)he->h_addr);
-    //server.sin_addr.s_addr = inet_addr(argv[1]);
+    //server.sin_addr = *((struct in_addr *)he->h_addr);
+    server.sin_addr.s_addr = inet_addr(argv[1]);
     server.sin_port = htons(port);// el puerto que usare (el segundo argumento de la llamada)
     bzero(&(server.sin_zero),8);
 
     //socket del cliente
-    id = socket(AF_INET, SOCK_STREAM, 8);
+    id = socket(AF_INET, SOCK_STREAM, 0);
     if( id == -1){
         printf("No se pudo crear socket\n");
     }
-    puts("Socket creado\n");
+    //puts("Socket creado\n");
 
     if (connect(id , (struct sockaddr *)&server , sizeof(server)) < 0)
 	{
 		perror("Fallo la conexion. Error\n");
 		return 1;
 	}
-    puts("Conectado\n");
+    //puts("Conectado\n");
     // ya en teoria la conexion al server esta hecha
-    write(id, buffer, 100);
+
+    while(1){
+        printf("Escribir mensaje: ");
+        scanf("%s", buffer);
+
+        if( send(id, buffer, strlen(buffer) , 0) < 0){
+            puts("Envio fallido");
+            return 1;
+        }
+
+        if(strcmp(buffer, "EXIT") == 0){
+            close(id);
+            return 1;
+        }
+        memset(buffer, 0, 500);
+    }
+    //write(id, buffer, 100);
     close(id); //Importante cerrar la conexion
 
 }
--- servidor.c
+++ servidor.c
@@ -4,6 +4,7 @@
 #include<arpa/inet.h> //inet_addr
 #include<unistd.h>    //write
 #include <netdb.h>
+#include <string.h> 
 
 //#define PORT 3006  // De verdad no hacer esto, tratar de no definir nada
 #define BACKLOG 2
@@ -14,7 +15,7 @@ int main(int argc, char *argv[]){
     sscanf(argv[1], "%d", &port);
     int id, new_id; // descriptores servidor, cliente
     int tam;
-    char buffer[100];
+    char buffer[500];
     struct sockaddr_in server , client;
     //estructura server del SERVIDOR
 
@@ -28,25 +29,36 @@ int main(int argc, char *argv[]){
     if( id == -1){
         printf("No se ha podido crear el socket\n");
     }
-    puts("Socket creado");
+    //puts("Socket creado");
 
     if( bind(id, (struct sockaddr *)&server , sizeof(server)) < 0){
         perror("Bind fallo. Error\n");
         return 1;
     }
-    puts("Bind listo\n");
+    //puts("Bind listo\n");
     
     listen(id, BACKLOG); // comienzo a escuchar
 
-    puts("Esperando por conecciones...\n");
+    puts("Esperando por conexiones...\n");
+
+    tam = sizeof(struct sockaddr_in);
+    new_id = accept(id, (struct sockaddr *)&client, (socklen_t*)&tam); // si llega algo lo acepto
+    printf("Se obtuvo una conexion desde %s\n", inet_ntoa(client.sin_addr)); // Asi se obtiene informacion de red del cliente, podrian aplicar otra cosa
+
+    int read_size;
 
     while(1){
-        tam = sizeof(struct sockaddr_in);
-        new_id = accept(id, (struct sockaddr *)&client, (socklen_t*)&tam); // si llega algo lo acepto
-        printf("Se obtuvo una conexion desde %d\n", inet_ntoa(client.sin_addr)); // Asi se obtiene informacion de red del cliente, podrian aplicar otra cosa
-        read(new_id, buffer, 100);
+        
+        read(new_id, buffer, 500);
+
+        if(strcmp(buffer, "EXIT") == 0){
+            printf("%s\n", buffer);
+            close(new_id);
+            return 1;
+        }
         printf("\n");
         printf("Mensaje del servidor: %s\n", buffer);
+        memset(buffer, 0, 500);
     }
     close(new_id);
 }
