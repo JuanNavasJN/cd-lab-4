f888b914f23896651f42bfb71e03d8a152cc39fc
danacp <33608962+danacp@users.noreply.github.com>
2018-12-16 22:42:34 -0400

    threadchat-done

--- cliente1.c
+++ cliente1.c
@@ -6,16 +6,65 @@
 #include <stdlib.h>
 #include<unistd.h>    //write
 #include <string.h>
+#include <pthread.h>
+//ESTRUCUTRA PARA SOCKET-THREAD
+struct client_socket_thread{
+        char buffer [256],nombre1 [256],nombre2 [256];
+        int sockfd;
+        pthread_t tid;
+        pthread_attr_t attr;
+        int rw;
+};
+
+void error(char *msg){
+        perror(msg);
+        exit(1);
+}
+
+void* client_runner_fun(void* args){
+
+        int n;
+        struct client_socket_thread *car = (struct client_socket_thread*) args;
+        if(car->rw == 0){
+          //HILO QUE ESCUCHA
+        while(1){
+                bzero(car->buffer, 256);
+                n = read(car->sockfd, car->buffer, 255);
+                if (n < 0){
+                        error("ERROR reading from socket");
+                }
+                if ((strcmp(car->buffer,"EXIT\n") == 0)||(strcmp(car->buffer,"EXIT ") == 0)){
+                  break;
+                }
+                printf("%s dice: %s\n",car->nombre2, car->buffer);
+
+                }
+        } else {
+          //HILO QUE ENVIA
+        while(1){
+                bzero(car->buffer, 256);
+                fgets(car->buffer, 255, stdin);
+                n = write(car->sockfd, car->buffer, strlen((char *) &(car->buffer)));
+                if ((strcmp(car->buffer,"EXIT\n") == 0)||(strcmp(car->buffer,"EXIT ") == 0)){
+                  break;
+                }
+                if (n < 0){
+                        error("ERROR writing to socket");
+                }
+
+        }
+        }
+}
 
 int main (int argc, char *argv[]){
     int id; //Descriptores servidor, cliente
     char buffer[500];
     char nombre[500];
     char usuariochat[500];
-
+    struct client_socket_thread client_write_t, client_read_t;
     struct sockaddr_in server; //int servidor
-
     struct hosten *he; // lo necesitare para el nombre de host o la ip
+
 
     int port;
     sscanf(argv[2], "%d", &port);
@@ -28,52 +77,39 @@ int main (int argc, char *argv[]){
     bzero(&(server.sin_zero),8);
 
     //socket del cliente
-    id = socket(AF_INET, SOCK_STREAM, 0);
-    if( id == -1){
+    client_write_t.sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    bcopy(&client_write_t.sockfd, &client_read_t.sockfd, sizeof(client_write_t.sockfd));//MEMSET
+    if( client_write_t.sockfd == -1){
         printf("No se pudo crear socket\n");
     }
-    //puts("Socket creado\n");
 
-    if (connect(id , (struct sockaddr *)&server , sizeof(server)) < 0)
-	{
-		perror("Fallo la conexion. Error\n");
-		return 1;
-	}
-    //puts("Conectado\n");
-    // ya en teoria la conexion al server esta hecha
-    printf("Escriba su nombre de usuario:  ");
-    scanf("%s", nombre);
-    send(id, nombre, strlen(nombre) , 0);
+    if (connect(client_write_t.sockfd , (struct sockaddr *)&server , sizeof(server)) < 0){
+		    perror("Fallo la conexion. Error\n");
+		    return 1;
+	  }
 
-    printf("Comienza a chatear\n");
+    printf("Escriba su nombre de usuario: \n");
+    fgets(nombre,500,stdin);
+    send(client_write_t.sockfd, nombre, (strlen(nombre)) , 0);
+    printf("Comienza a chatear: \n");
+    memset(nombre, 0, strlen(nombre));
+    read(client_write_t.sockfd, nombre, 255);
+    strncpy(client_read_t.nombre2,nombre,strlen(nombre)-1);
+    //printf("TAMANO: %d", strlen(client_read_t.nombre2));
 
-    while(1){
+    sleep(1);//SINCRONIZACION CON SERVIDOR
 
-        memset(buffer, 0, 500);
+    pthread_attr_init(&client_write_t.attr);
+    pthread_attr_init(&client_read_t.attr);
+    client_write_t.rw = 1;
+    client_read_t.rw = 0;
+    //HILO QUE ENVIA
+    pthread_create(&client_write_t.tid, &client_write_t.attr, client_runner_fun, &client_write_t);
+    //HILO QUE RECIBE
+    pthread_create(&client_read_t.tid, &client_read_t.attr, client_runner_fun, &client_read_t);
+    pthread_join(client_write_t.tid, NULL);
+    pthread_join(client_read_t.tid, NULL);
 
-        printf("<-- ");
+    return 0;
 
-        scanf("%s", buffer);
-
-        if( send(id, buffer, strlen(buffer) , 0) < 0){
-            puts("Envio fallido");
-            return 1;
-        }else{
-             //printf("<-- ");
-        }
-        memset(buffer, 0, 500);
-        
-        do{
-            recv(id, buffer, 500, 0);
-        }while(!strcmp(buffer, ""));
-        
-        printf("%s\n", buffer);
-
-        if(strcmp(buffer, "EXIT") == 0){
-            close(id);
-            return 1;
-        }
-        
-    }
-    close(id); //Importante cerrar la conexion
 }
--- servidor.c
+++ servidor.c
@@ -1,23 +1,77 @@
-#include<stdio.h>
-#include<string.h>    //strlen
-#include<sys/socket.h>
-#include<arpa/inet.h> //inet_addr
-#include<unistd.h>    //write
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <netdb.h>
+#include<arpa/inet.h>	//inet_addr
+#include<stdio.h>	//printf
+#include <stdlib.h>
+#include<unistd.h>    //write
 #include <string.h>
+#include <pthread.h>
+
 
 #define BACKLOG 3
+//ESTRUCUTRA PARA SOCKET-THREAD
+struct server_socket_thread{
+    char buffer [256],nombre1 [256],nombre2 [256];
+    int newsockfd,newsockfd2;
+    pthread_t tid;
+    pthread_attr_t attr;
+        int rw,rw2;
+};
+
+void error(char *msg){
+        perror(msg);
+        exit(1);
+}
+
+//FUNCION QUE SIRVE DE RECEPTOR Y ENVIO
+void* server_recibe_envia(void* args){
+    int n;
+    struct server_socket_thread *sar = (struct server_socket_thread*) args;
+    if(sar->rw == 0){
+      //HILO QUE ESCUCHA A CLIENTE2 Y ENVIA A CLIENTE1
+      while(1){
+        bzero(sar->buffer, 256);
+        n = read(sar->newsockfd2, sar->buffer, 255);
+        if (n < 0){
+  //          error("ERROR reading from socket");
+        }
+        write(sar->newsockfd, sar->buffer, strlen((char *) &(sar->buffer)));
+        if ((strcmp(sar->buffer,"EXIT\n") == 0)||(strcmp(sar->buffer,"EXIT ") == 0)){
+          printf("Salio %s", sar->nombre2);
+          close(sar->newsockfd);
+          break;
+        }
+        }
+    } else {
+      //HILO QUE ESCUCHA AL CLIENTE1 Y ENVIA A CLIENTE2
+        while(1){
+          bzero(sar->buffer, 256);
+          n = read(sar->newsockfd, sar->buffer, 255);
+          if (n < 0){
+  //          error("ERROR reading from socket");
+          }
+          write(sar->newsockfd2, sar->buffer, strlen((char *) &(sar->buffer)));
+          if ((strcmp(sar->buffer,"EXIT\n") == 0)||(strcmp(sar->buffer,"EXIT ") == 0)){
+            printf("Salio %s", sar->nombre1);
+            close(sar->newsockfd2);
+            break;
+          }
+        }
+    }
+}
 
 int main(int argc, char *argv[]){
 
     int port;
     sscanf(argv[1], "%d", &port);
-    int id, new_id[2], i; // descriptores servidor, cliente
+    int id, new_id, new_id2; // descriptores servidor, cliente
     int tam;
     char buffer[500];
-    char mensaje[700];
-    struct sockaddr_in server , client[2];
-    char usuario1[20], usuario2[20];
+    char buffer2[500];
+    struct sockaddr_in server , client;
+    struct server_socket_thread server_write_t, server_read_t;
+
     //estructura server del SERVIDOR
 
     server.sin_family = AF_INET;
@@ -25,70 +79,64 @@ int main(int argc, char *argv[]){
     server.sin_port = htons(port); // el puerto que usare
     bzero(&(server.sin_zero), 8);
 
-    //socket del servidor
     id = socket(AF_INET, SOCK_STREAM, 0);
     if( id == -1){
         printf("No se ha podido crear el socket\n");
     }
-    //puts("Socket creado");
 
     if( bind(id, (struct sockaddr *)&server , sizeof(server)) < 0){
         perror("Bind fallo. Error\n");
         return 1;
     }
-    //puts("Bind listo\n");
 
     puts("Esperando por conexiones...\n");
     tam = sizeof(struct sockaddr_in);
 
     listen(id, 3); // comienzo a escuchar
 
-    for(i = 0; i < 2; i++){
+    server_write_t.newsockfd = accept(id, (struct sockaddr *)&client, (socklen_t*)&tam); // si llega algo lo acepto
+    printf("Se obtuvo una conexion desde %s\n", inet_ntoa(client.sin_addr)); // Asi se obtiene informacion de red del cliente, podrian aplicar otra cosa
 
-        new_id[i] = accept(id, (struct sockaddr *)&client[i], (socklen_t*)&tam); // si llega algo lo acepto
-        printf("Se obtuvo una conexion desde %s\n", inet_ntoa(client[i].sin_addr)); // Asi se obtiene informacion de red del cliente, podrian aplicar otra cosa
-    }
+    server_write_t.newsockfd2= accept(id, (struct sockaddr *)&client, (socklen_t*)&tam); // si llega algo lo acepto
+    printf("Se obtuvo una conexion desde %s\n", inet_ntoa(client.sin_addr));
 
-    for(i = 0; i < 2; i++){
-         read(new_id[i], buffer, 500);
-         write(new_id[i], "", 1);
-         printf("%s se ha conectado\n", buffer);
-
-        if(i == 0){
-            strcpy(usuario1, buffer);
-        }else {
-            strcpy(usuario2, buffer);
-        }
         memset(buffer, 0, 500);
-    }
-    int rec;
-    while(1){
+        memset(buffer2, 0, 500);
+        //RECIBE NOMBRES
+        recv(server_write_t.newsockfd, buffer, (sizeof(buffer)-1), 0);
+        strncpy(server_write_t.nombre1,buffer,strlen(buffer));
+        printf ("Se conecta %s", buffer);
+        send(server_write_t.newsockfd2, buffer, (strlen(buffer)) , 0);
 
-        rec = recv(new_id[0], buffer, sizeof(buffer), MSG_PEEK);
+        recv(server_write_t.newsockfd2, buffer2, (sizeof(buffer2)-1), 0);
+        strncpy(server_read_t.nombre2,buffer2,strlen(buffer2));
+        printf ("Se conecta %s", buffer2);
+        send(server_write_t.newsockfd, buffer2, (strlen(buffer2)) , 0);
 
-        // if(rec <= 0){
-        //     printf("%s se ha desconectado\n", usuario1);
-        // }
-        
-        sprintf(mensaje, "--> %s dice: %s", usuario1, buffer);
-        send(new_id[1], mensaje, sizeof(mensaje), 0);
-        //printf("enviado a %d\n", new_id[1]);
-        memset(buffer, 0, 500);
-        memset(mensaje, 0, 700);
+      //  memset(buffer, 0, 500);
+      //  memset(buffer2, 0, 500);
 
-        rec = recv(new_id[1], buffer, sizeof(buffer), MSG_PEEK);
+        //INICIO HILOS
+        pthread_attr_init(&server_write_t.attr);
+        pthread_attr_init(&server_read_t.attr);
+        server_write_t.rw = 1;
+        server_read_t.rw = 0;
+        bcopy(&server_write_t.newsockfd, &server_read_t.newsockfd, sizeof(server_write_t.newsockfd));
+        bcopy(&server_write_t.newsockfd2, &server_read_t.newsockfd2, sizeof(server_write_t.newsockfd2));
+        //ESCUCHA AL CLIENTE 1 Y ENVIA AL CLIENTE 2:
+        pthread_create(&server_write_t.tid, &server_write_t.attr, server_recibe_envia, &server_write_t);
+        //ESCUCHA AL CLIENTE 2 Y ENVIA AL CLIENTE 1:
+        pthread_create(&server_read_t.tid, &server_read_t.attr, server_recibe_envia, &server_read_t);
 
-        // if(rec <= 0){
-        //     printf("%s se ha desconectado\n", usuario1);
-        // }
+        pthread_join(server_write_t.tid, NULL);
+        //printf("SALE1\n");
+        pthread_join(server_read_t.tid, NULL);
+        //printf("SALE2\n");
 
-        sprintf(mensaje, "--> %s dice: %s", usuario2, buffer);
-        send(new_id[0], mensaje, sizeof(mensaje), 0);
-        //printf("enviado a %d\n", new_id[0]);
-        memset(buffer, 0, 500);
-        memset(mensaje, 0, 700);
 
-    }
-    close(new_id[0]);
-    close(new_id[1]);
+
+        return 0;
+
+
+
 }
